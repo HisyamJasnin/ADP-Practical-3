dc579fb4e380b54a4ebc4dbe074444784dd698e2
Sam <19FTT1067@student.edu.bn>
2021-03-17 09:13:45 +0800

    Lesson 1.3 - Title, Score and Previous Score details

--- package01/BabyBirdGame.java
+++ package01/BabyBirdGame.java
@@ -3,9 +3,12 @@ package package01;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class BabyBirdGame extends Application{
@@ -22,11 +25,33 @@ public class BabyBirdGame extends Application{
 		arg0.show();
 	}
 	
+	private static int score =0;
+	private static int previousScore= 0;
+	
 	public static void initGUI() {
+		previousScore = score;
+		score = 0;
 		VBox rootPane = new VBox();
 		 rootPane.setAlignment(Pos.TOP_CENTER);
 		 rootPane.setBackground(new Background(new BackgroundFill(Color.BLACK,
 		null, null)));
+		 
+		 // Title
+		 Label titleLabel = new Label("Baby Bird");
+		 titleLabel.setFont(Font.font(32));
+		 titleLabel.setTextFill(Color.WHITE);
+		 
+		 // Score
+		 ScorePane scorePane = new ScorePane("Score: ", score, Color.DARKBLUE, Color.WHITE);
+		 
+		 // Previous Score
+		 ScorePane previousScorePane = new ScorePane("Previous Score:", previousScore, Color.DARKCYAN, Color.WHITE);
+		 
+		 // Bird Flight
+		 
+		 //Help Message
+		 
+		 rootPane.getChildren().addAll(titleLabel, scorePane, previousScorePane);
 
 		 Scene scene = new Scene(rootPane);
 
--- package01/ScorePane.java
+++ package01/ScorePane.java
@@ -1,5 +1,30 @@
 package package01;
 
-public class ScorePane {
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class ScorePane extends HBox{
+	private Label scoreTitleLabel, scoreLabel;
+	
+	public ScorePane(String title, int score, Color backgroundColour, Color textColour) {
+		scoreTitleLabel = new Label(title);
+		scoreTitleLabel.setFont(Font.font(14));
+		scoreTitleLabel.setTextFill(textColour);
+		
+		scoreLabel = new Label("" + score);
+		scoreLabel.setFont(Font.font("Serif", FontWeight.BOLD, 24));
+		scoreLabel.setTextFill(textColour);
+		
+		this.getChildren().addAll(scoreTitleLabel, scoreLabel);
+		this.setSpacing(10);
+		this.setAlignment(Pos.CENTER);
+		this.setBackground(new Background(new BackgroundFill(backgroundColour, null, null)));
+	}
 
 }
