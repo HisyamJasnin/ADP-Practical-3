e518d88fa994815815891b429192f62a5a55d236
Sam <19FTT1067@student.edu.bn>
2021-03-17 08:46:53 +0800

    Lesson 1.2 - Planning the game

--- package01/BabyBirdGame.java
+++ package01/BabyBirdGame.java
@@ -1,16 +1,37 @@
 package package01;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class BabyBirdGame extends Application{
+	private static Stage primaryStage;
 
 	@Override
 	public void start(Stage arg0) throws Exception {
+		primaryStage = arg0;
+		initGUI();
+		
 		arg0.setTitle("Baby Bird");
 		arg0.setResizable(false);
 		arg0.sizeToScene();
 		arg0.show();
+	}
+	
+	public static void initGUI() {
+		VBox rootPane = new VBox();
+		 rootPane.setAlignment(Pos.TOP_CENTER);
+		 rootPane.setBackground(new Background(new BackgroundFill(Color.BLACK,
+		null, null)));
+
+		 Scene scene = new Scene(rootPane);
+
+		 primaryStage.setScene(scene);
+		
 	}
 	
 	public static void main(String[] args) {
